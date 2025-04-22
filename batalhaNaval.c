16699f34c9726ca0a652bb27b5fefc47cc205d9e
Mateus Belfort <93524398+Mateus-Belfort@users.noreply.github.com>
2025-04-22 20:22:11 -0300

    nivel aventureiro.c
    adicionado no código as especificidades solicitadas para o código, como a inclusão de mais dois navios na diagonal e mais uma avalização de condição se os numeros inclusos estavam, dentro do valor estipulado.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,34 +10,45 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    
-    int matriz[10][10] = {  //matriz com o tabuleiro e localização dos navios
-       {0,0,0,0,0,0,0,0,0,0}
-      ,{0,3,0,0,0,0,0,0,0,0}
-      ,{0,3,0,0,0,0,0,0,0,0}
-      ,{0,3,0,0,0,0,0,0,0,0}
-      ,{0,0,0,0,0,3,3,3,0,0}
-      ,{0,0,0,0,0,0,0,0,0,0}
-      ,{0,0,0,0,0,0,0,0,0,0}
-      ,{0,0,0,0,0,0,0,0,0,0}
-      ,{0,0,0,0,0,0,0,0,0,0}
-      ,{0,0,0,0,0,0,0,0,0,0}
-    }, i,j;
-
-    printf("\n###################Tabuleiro de batalha naval###################\n");
-    
-    for ( i = 0; i < 10; i++){ //laço externo para linhas
-      for ( j = 0; j < 10; j++){  // laço interno para colunas
-        printf("%d ",matriz[i][j]);
-      }
-      printf("\n");      
-    }
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    
+    int matriz[10][10] = {  //matriz com o tabuleiro e localização dos navios
+       {0,0,0,0,3,0,0,0,0,0}
+      ,{0,3,0,0,0,3,0,0,0,0}
+      ,{0,3,0,0,0,0,3,0,0,0}
+      ,{0,3,0,0,0,0,0,0,0,0}
+      ,{0,0,0,0,0,3,3,3,0,0}
+      ,{0,0,0,0,0,0,0,0,0,0}
+      ,{0,0,0,0,3,0,0,0,0,0}
+      ,{0,0,0,3,0,0,0,0,0,0}
+      ,{0,0,3,0,0,0,0,0,0,0}
+      ,{0,3,0,0,0,0,0,0,0,0}
+    }, i,j;
 
+    printf("\n###################Tabuleiro de batalha naval###################\n");  // decoração para reprara o tabuleiro do indice superior, tornando o código mais limpot.
+    
+    for ( i = 0; i < 10; i++){ //laço externo para linhas
+      for ( j = 0; j < 10; j++){  // laço interno para colunas
+        printf("%d ",matriz[i][j]); // aqui ele vai imprimir a tabela com as matrizes.
+      }
+      printf("\n"); //aqui é incluso o \n para pular uma linha e ficar mais limpo.
+    }
+
+    printf("###############################################################\n"); 
+
+    if (i <= 10 && j <= 10){ // aqui ele verifica se a posição [0][0] é igual a 3, ou seja, se tem navio.
+      printf("Navios dentro da posição estipulada\n"); // aqui ele imprime que o navio foi encontrado na posição [0][0].
+    }
+     else 
+    {
+      printf("Navios fora da posição estipulada\n"); // aqui ele imprime que o navio não foi encontrado na posição [0][0].    
+    }
+    
+
+   
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
